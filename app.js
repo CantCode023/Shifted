4fa584f2ac806012d335efc28efefee925d8cac3
CantCode <cantcode023@gmail.com>
2021-11-27 17:56:58 +0800

    updated files

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require("express")
 const app = express()
 const port = 8080
 
-// Load Stuffs
-app.use('/css', express.static(__dirname + '/public/style'))
+// Load CSS
+app.use('/css', express.static(__dirname + '/public/css'))
+// Load JavaScript
 app.use('/js', express.static(__dirname + '/public/js'))
+// Load Images
+app.use('/images', express.static(__dirname + '/public/images'))
 
-// EJS
+// EJS Viewer Setup
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
@@ -14,10 +17,17 @@ app.get('/', function(req,res) {
     res.render('index')
 })
 
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+function shift(offset=null, message=null) {
+    console.log(alphabet)
+}
+
 app.get('/encode/:string', function(req,res) {
     let string = req.params.string
 
-    
+    shift()
 })
 
 app.listen(port)
